af49c9c6e274bc21188b4eef75761ab28cc1155e
Vitaly Lyapunov <stokwell1@gmail.com>
2017-07-10 13:18:57 +0200

    S3 for heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,33 @@
 require 'carrierwave'
 require 'carrierwave/storage/fog'
 
+
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws',
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id:ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key:ENV["AWS_SECRET_ACCESS_KEY"],
-    region:'eu-central-1'
-  }
-  config.fog_directory =ENV["AWS_S3_BUCKET"]
+  if Rails.env.development? || Rails.env.test?
+    CarrierWave.configure do |config|
+      config.fog_provider = 'fog/aws',
+      config.fog_credentials = {
+        provider: 'AWS',
+        aws_access_key_id:ENV["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key:ENV["AWS_SECRET_ACCESS_KEY"],
+        region:'eu-central-1'
+      }
+      config.fog_directory =ENV["AWS_S3_BUCKET"]
+    end
+  end
+
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.fog_provider = 'fog/aws',
+      config.fog_credentials = {
+        provider: 'AWS',
+        aws_access_key_id: "AWS_ACCESS_KEY_ID",
+        aws_secret_access_key: "AWS_SECRET_ACCESS_KEY",
+        region:'eu-central-1'
+      }
+      config.fog_directory = "AWS_S3_BUCKET"
+    end
+  end
+
+
 end
